045841516ebe13fa6962b018bb8930b7d7914fa8
madaraDance <maksim.minenko97@gmail.com>
2024-04-10 14:14:13 +0300

    deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import '../dist/assets/index--HIKf8NQ.js';
-import '../dist/assets/index-C0UuUAZC.css';
 
 const root = createRoot(document.getElementById('root'));
 
