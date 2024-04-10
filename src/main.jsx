caa7c4a015650a140e61d68f95a3aae8de091b19
madaraDance <maksim.minenko97@gmail.com>
2024-04-10 14:01:02 +0300

    please work

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import '/assets/index--HIKf8NQ.js';
+import '/assets/index-C0UuUAZC.css';
 
 const root = createRoot(document.getElementById('root'));
 
